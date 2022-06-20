60311b47fa2339ac4c52326552ae9ea1c819a773
Alejandro Echavarria <mechavarria@inespre.gob.do>
2022-06-20 15:23:57 -0400

    mechavarria-2022-06-20-work

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import freeCodeCampLogo from './images/freecodecamp_logo.png';
 import Button from './components/Buttons';
 import Screen from './components/Screen';
 import ClearButton from './components/ClearButton';
+import {useState} from 'react';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    
+    setInput(input + val);
+  };
+
   return (
     <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
@@ -14,33 +23,33 @@ function App() {
           alt='Logo de freecodecamp' />
       </div>
       <div className='contenedor-calculadora'>
-        <Screen />
+        <Screen input={input}/>
         <div className='fila'>
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button>x</Button>
+          <Button manejarClick={agregarInput}>7</Button>
+          <Button manejarClick={agregarInput}>8</Button>
+          <Button manejarClick={agregarInput}>9</Button>
+          <Button manejarClick={agregarInput}>x</Button>
         </div>
         <div className='fila'>
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>-</Button>
+          <Button manejarClick={agregarInput}>4</Button>
+          <Button manejarClick={agregarInput}>5</Button>
+          <Button manejarClick={agregarInput}>6</Button>
+          <Button manejarClick={agregarInput}>-</Button>
         </div>
         <div className='fila'>
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>+</Button>
+          <Button manejarClick={agregarInput}>1</Button>
+          <Button manejarClick={agregarInput}>2</Button>
+          <Button manejarClick={agregarInput}>3</Button>
+          <Button manejarClick={agregarInput}>+</Button>
         </div>
         <div className='fila'>
-          <Button>.</Button>
-          <Button>0</Button>
-          <Button> = </Button>
-          <Button>/</Button>
+          <Button manejarClick={agregarInput}>.</Button>
+          <Button manejarClick={agregarInput}>0</Button>
+          <Button manejarClick={agregarInput}> = </Button>
+          <Button manejarClick={agregarInput}>/</Button>
         </div>
         <div className='fila'>
-            <ClearButton>
+            <ClearButton manejarClear={() => setInput('')}>
               CE
             </ClearButton>
         </div>
--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -1,8 +1,9 @@
 import React from "react";
+import '../stylesheets/ClearButton.css';
 
 const ClearButton = (props) => (
 
-    <div className='boton-clear'>
+    <div className='boton-clear' onClick={props.manejarClear}>
         {props.children}
     </div>
 );
